4635682425296ad6343fa79b3df98cbc0145931a
buisnessmind7777-blip <buisnessmind7777@gmail.com>
2026-08-22 00:35:51 +0500

    Update model version in server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post("/api/chat", async (req, res) => {
         res.flushHeaders();
 
         const response = await client.responses.create({
-    model: "gpt-5.6",
+    model: "gpt-5.6-luna",
     input: message
 });
 
